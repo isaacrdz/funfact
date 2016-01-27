be1e0e1b0c0893831cc84e377cab0d1a4d9b1873
Isaacrdz <zack_cpp@hotmail>
2016-01-27 16:57:40 -0600

    Creating a Datta Collection

--- FunFacts/ViewController.swift
+++ FunFacts/ViewController.swift
@@ -12,10 +12,11 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var funFactLabel: UILabel!
     
-    
+    let facts = [ "Ants stretch when they wake up in the morning", "Ostriches can run faster than horses"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        funFactLabel.text = "An Interesting fact!"
+        funFactLabel.text = facts[0]
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,7 +25,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showFunFact() {
-        funFactLabel.text = "Another interesting fact"
+        funFactLabel.text = facts[1]
     }
 }
 
